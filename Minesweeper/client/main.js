6e477d029a2dac2d7a9c42893609a3fab2772dbf
DavidNHill <david.hill.515@outlook.com>
2025-01-11 18:58:38 +0000

    Do full BFDA when hot key 'a' is pressed

--- Minesweeper/client/main.js
+++ Minesweeper/client/main.js
@@ -1666,7 +1666,7 @@ function keyPressedEvent(e) {
     let newValue = null;
     if (e.key == 'a') {
         if (!analysisButton.disabled) {  // don't allow the hotkey if the button is disabled
-            doAnalysis();
+            doAnalysis(true);
         }
 
     } else if (analysisMode) {
