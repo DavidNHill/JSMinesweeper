3832323dd04c1bf7a88f7d8aec74d9cb5176e5c3
DavidNHill <david.hill.515@outlook.com>
2025-01-15 16:37:44 +0000

    Don't mark tiles as newly discovered mines until all solver steps have been completed.  Otherwise state inconsistencies can occur.

--- Minesweeper/client/solver_main.js
+++ Minesweeper/client/solver_main.js
@@ -371,7 +371,7 @@ async function solver(board, options) {
 
         for (let tile of pe.minesFound) {   // place each found flag
             tile.setProbability(0);
-            tile.setFoundBomb();
+            //tile.setFoundBomb();
             //if (options.playStyle == PLAY_STYLE_FLAGS) {
                 const action = new Action(tile.getX(), tile.getY(), 0, ACTION_FLAG);
                 result.push(action);
@@ -391,14 +391,14 @@ async function solver(board, options) {
             // find some witnesses which can be adjusted to remove the guessing
             findBalancingCorrections(pe);
 
-            return result;
+            return addDeadTiles(result, pe.getDeadTiles(), pe.minesFound);
         }
 
         // if we aren't allowing advanced guessing then stop here
         if (!options.advancedGuessing) {
             writeToConsole("Advanced guessing is turned off so exiting the solver after the probability engine");
             showMessage("Press 'Analyse' for advanced guessing");
-            return result;
+            return addDeadTiles(result, pe.getDeadTiles(), pe.minesFound);
         }
 
         /*
@@ -474,7 +474,7 @@ async function solver(board, options) {
                         deadTiles.push(deadTile);
                     }
                 }
-                return addDeadTiles(result, deadTiles);
+                return addDeadTiles(result, deadTiles, pe.minesFound);
             }
 
         }
@@ -533,7 +533,7 @@ async function solver(board, options) {
                     deadTiles = allCoveredTiles;   // all the tiles are dead
                 }
 
-                return addDeadTiles(result, deadTiles);
+                return addDeadTiles(result, deadTiles, pe.minesFound);
             } else {
                 deadTiles = pe.getDeadTiles();  // use the dead tiles from the probability engine
                 partialBFDA = bfda;
@@ -579,7 +579,7 @@ async function solver(board, options) {
                     showMessage(recommended.asText() + " is an unavoidable 50/50 guess, or safe." + formatSolutions(pe.finalSolutionsCount));
                 }
                 //showMessage(recommended.asText() + " is an unavoidable 50/50 guess, or safe." + formatSolutions(pe.finalSolutionsCount));
-                return addDeadTiles(result, pe.getDeadTiles());
+                return addDeadTiles(result, pe.getDeadTiles(), pe.minesFound);
             }
         }
 
@@ -605,7 +605,7 @@ async function solver(board, options) {
                     showMessage(recommended.asText() + " is an unavoidable 50/50 guess, or safe." + formatSolutions(pe.finalSolutionsCount));
                 }
 
-                return addDeadTiles(result, pe.getDeadTiles());
+                return addDeadTiles(result, pe.getDeadTiles(), pe.minesFound);
             }
         }
 
@@ -669,13 +669,17 @@ async function solver(board, options) {
 
         }
 
-        //return addDeadTiles(result, pe.getDeadTiles());
-        return addDeadTiles(result, deadTiles);
+        return addDeadTiles(result, deadTiles, pe.minesFound);
 
     }
 
     // used to add the dead tiles to the results
-    function addDeadTiles(result, deadTiles) {
+    // also used to mark the found mines
+    function addDeadTiles(result, deadTiles, mines) {
+
+        for (let tile of mines) {   //mark each found mine
+            tile.setFoundBomb();
+        }
 
         // identify the dead tiles
         for (let tile of deadTiles) {   // show all dead tiles 
