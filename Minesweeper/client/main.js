64d70224fb6329152e2ae33b5b3f881bde455b4c
Nice Zombies <nineteendo19d0@gmail.com>
2025-11-06 13:46:50 +0100

    Highlight dead tiles

--- Minesweeper/client/main.js
+++ Minesweeper/client/main.js
@@ -823,12 +823,17 @@ function renderHints(hints, otherActions, drawOverlay) {
 
         if (hint.action == ACTION_CHORD) {
             ctxHints.fillStyle = "#00FF00";
+            firstGuess = 2;
         } else if (hint.prob == 0) {   // mine
             ctxHints.fillStyle = "#FF0000";
         } else if (hint.prob == 1) {  // safe
             ctxHints.fillStyle = "#00FF00";
+            firstGuess = 2;
         } else if (hint.dead) {  // uncertain but dead
             ctxHints.fillStyle = "black";
+            if (firstGuess == 0) {
+                firstGuess = 1;
+            }
         } else {  //uncertain
             ctxHints.fillStyle = "orange";
             if (firstGuess == 0) {
